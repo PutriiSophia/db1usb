9746f22c0d3bbea839e12dce846fbf92a978a588
PutriiSophia <100389057+PutriiSophia@users.noreply.github.com>
2022-03-13 22:34:46 +0800

    Add files via upload

---
+++ Putri Sophia.py
@@ -0,0 +1,49 @@
+#Tipe data pada Phyton
+
+#Boolean
+print(1<3) #pembuktian false atau true
+print(2>5)
+
+#String
+print("I am string")
+print('ayo belajar Phyton')
+
+#Integer
+a = 7 #menggunakan variabel
+print(a)
+
+#Float
+0.4 = .4 #contoh penulisan koma pada float
+4.0 = 4.
+a = 4.1 #menggunakan variabel
+print(a)
+
+
+#Operator Aritmatika
+
+#menggunakan 2 variabel
+nilai1 = 5
+nilai2 = 7
+print(nilai1 + nilai2)
+
+#menggunakan inputan user
+nilai1 = int(input("masukkan nilai1 :"))
+nilai2 = int(input("masukkan nilai2 :"))
+
+#operator
+penjumlahan = nilai1 + nilai2
+pengurangan = nilai1 - nilai2
+perkalian = nilai1 * nilai2
+pembagian = nilai1 / nilai2
+sisa_bagi = nilai1 % nilai2
+pangkat = nilai1 ** nilai2
+pembagian_bulat = nilai1 // nilai2
+
+#output
+print(nilai1, '+', nilai2, '=', penjumlahan)
+print(nilai1, '-', nilai2, '=', pengurangan)
+print(nilai1, '*', nilai2, '=', perkalian)
+print(nilai1, '/', nilai2, '=', pembagian)
+print(nilai1, '%', nilai2, '=', sisa_bagi)
+print(nilai1, '**', nilai2, '=', pangkat)
+print(nilai1, '//', nilai2, '=', pembagian_bulat)
